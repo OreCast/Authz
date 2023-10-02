88fc8f46b6a5d517fdcb213f8e4bba921321707c
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-02 10:35:34 -0400

    Add autoIncrement for user ID

--- users.go
+++ users.go
@@ -10,7 +10,7 @@ import (
 
 // User represents user table
 type User struct {
-	ID         uint   `json:"id" gorm:"primaryKey"`
+	ID         uint   `json:"id" gorm:"primaryKey,autoIncrement	"`
 	LOGIN      string `json:"login"`
 	FIRST_NAME string `json:"first_name"`
 	LAST_NAME  string `json:"last_name"`
